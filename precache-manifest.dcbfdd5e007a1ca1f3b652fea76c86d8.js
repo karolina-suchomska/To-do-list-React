04f7e5de07cd497b2c3ae6cdeaf5b4260e1712c2
unknown <xrage6@gmail.com>
2019-05-20 23:19:44 +0200

    Updates

---
+++ precache-manifest.dcbfdd5e007a1ca1f3b652fea76c86d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13635d5d0b52581f2bf6a972960cdc95",
+    "url": "/To-do-list-React/index.html"
+  },
+  {
+    "revision": "ae9d0da3110ee2a16c93",
+    "url": "/To-do-list-React/static/css/main.df3e1376.chunk.css"
+  },
+  {
+    "revision": "d9545d6d1085afad4153",
+    "url": "/To-do-list-React/static/js/2.3584e5c9.chunk.js"
+  },
+  {
+    "revision": "ae9d0da3110ee2a16c93",
+    "url": "/To-do-list-React/static/js/main.0d972268.chunk.js"
+  },
+  {
+    "revision": "f936ac51959b47601a82",
+    "url": "/To-do-list-React/static/js/runtime~main.124c3cf5.js"
+  }
+]);
